3446f5a2d295a9cf10e6c9a8d9e8c87baf833032
DJlazcano <jlazcanoaguilar@deloitte.com>
2024-08-14 17:48:54 -0600

    Book class and usecase

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -5,33 +5,66 @@ namespace Practice1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please choose an option: ");
-            Console.WriteLine("(1) BMI Calculator");
-            var option = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Please choose an option: ");
+                Console.WriteLine("(1) BMI Calculator");
+                Console.WriteLine("(2) Book Class");
+                var option = int.Parse(Console.ReadLine());
 
-            switch (option){
-                case 1:
-                    bmiCalculator();
+                switch (option)
+                {
+                    case 1:
+                        bmiCalculator();
 
-                    break;
-                case 2:
+                        break;
+                    case 2:
+                        List<Book> books = new List<Book>();
 
-                    break; 
-                case 3:
+                        Book book1 = new Book("Moby-Dick", "Herman Melville", "123987");
+                        Book book2 = new Book("The Lord of the Rings", "John Ronald", "569032");
+                        Book book3 = new Book("The Lord of the Rings", "Jhonny Wonka", "569032");
 
-                    break;
-                case 4:
+                        book1.bookDetails();
 
-                    break;
-                case 5:
+                        books.Add(book1);
+                        books.Add(book2);
+                        books.Add(book3);
+                        Console.WriteLine("List of books:");
+                        books.ForEach(p => p.bookDetails());
 
-                    break;
-                case 6:
+                        Console.WriteLine("After Removing 1 element:");
+                        books.RemoveAt(0);
+                        books.ForEach(p => p.bookDetails());
 
-                    break;
-                default:
-                    break;
-                }
+                        Console.WriteLine("\nSingle book from list:");
+                        Console.WriteLine(books[1]);
+
+                        Console.WriteLine("\nCompare two books by ISBN: ");
+                        Console.WriteLine("Book1 isbn({0}) equals book2 isbn({1})?: {2}\n", books[0].getIsbn(),
+                            books[1].getIsbn(),
+                            books[0].Equals(books[1]));
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    default:
+                        break;
+                } 
+            } while (true);
         }
     }
 }
