dedd682f677ddad78f7f6ccc6696fb22df5d361e
DJlazcano <jlazcanoaguilar@deloitte.com>
2024-08-14 16:41:11 -0600

    BMI Calculator

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,11 +1,37 @@
 ﻿
 namespace Practice1
 {
-    class MultiProgram
+    class MultiProgram : BMICalculator
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please choose an option: ");
+            Console.WriteLine("(1) BMI Calculator");
+            var option = int.Parse(Console.ReadLine());
+
+            switch (option){
+                case 1:
+                    bmiCalculator();
+
+                    break;
+                case 2:
+
+                    break; 
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                default:
+                    break;
+                }
         }
     }
 }
