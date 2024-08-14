2b703f4e6f27a8e685efea72a0c8c37d843f230c
DJlazcano <jlazcanoaguilar@deloitte.com>
2024-08-14 17:49:49 -0600

    Book class

---
+++ Practice1/BMICalculator.cs
@@ -0,0 +1,32 @@
+﻿namespace Practice1
+{
+    internal class BMICalculator
+    {
+
+        public static void bmiCalculator()
+        {
+            Console.WriteLine("Please enter your weight in kilograms: ");
+            var weight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your height in meters: ");
+            var height = float.Parse(Console.ReadLine());
+
+            float bmi = calculateBmi(weight, height);
+
+            Console.WriteLine("Your BMI is: {0}, and your category is: {1}\n", bmi.ToString("#.##"),
+                bmi < 18.5 ? "Underweight" :
+                (bmi > 18.5 && bmi <= 24.9) ? "Normal Weight" :
+                (bmi > 25 && bmi <= 29.9) ? "Overweight" : "Obesity");
+        }
+
+        private static float calculateBmi(float weight, float height)
+        {
+            var bmi = weight / (height * height);
+            if (height > 2)
+            {
+                bmi *= 10000;
+            }
+
+            return bmi;
+        }
+    }
+}
---
+++ Practice1/Book.cs
@@ -0,0 +1,36 @@
+﻿namespace Practice1
+{
+    public class Book
+    {
+        string title;
+        string author;
+        string isbn;
+
+        public Book() { }
+        public Book(string title, string author, string isbn)
+        {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+        }
+
+        public string getIsbn() { return isbn; }
+        public string getTitle() { return title; }
+        public string getAuthor() { return author; }
+        public void bookDetails()
+        {
+            Console.WriteLine("Book Title: {0} Author: {1} ISBN: {2}", this.title, this.author, this.isbn);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var bookN = obj as Book;
+            return this.isbn.Equals(bookN.getIsbn());
+        }
+
+        public override string? ToString()
+        {
+            return "Book Title: " + this.title + " Author: "+ this.author + "ISBN: " + this.isbn;
+        }
+    }
+ }
