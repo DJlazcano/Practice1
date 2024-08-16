edd35b5cf831ced3f057d0848aa2d0478bf8dd63
DJlazcano <jlazcanoaguilar@deloitte.com>
2024-08-15 18:14:14 -0600

    File Sorter and Date compare

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -26,109 +26,132 @@ namespace Practice1
 
                         break;
                     case 2:
-                        List<Book> books = new List<Book>();
-
-                        Book book1 = new Book("Moby-Dick", "Herman Melville", "123987");
-                        Book book2 = new Book("The Lord of the Rings", "John Ronald", "569032");
-                        Book book3 = new Book("The Lord of the Rings", "Jhonny Wonka", "569032");
-
-                        book1.bookDetails();
-
-                        books.Add(book1);
-                        books.Add(book2);
-                        books.Add(book3);
-                        Console.WriteLine("List of books:");
-                        books.ForEach(p => p.bookDetails());
-
-                        Console.WriteLine("After Removing 1 element:");
-                        books.RemoveAt(0);
-                        books.ForEach(p => p.bookDetails());
-
-                        Console.WriteLine("\nSingle book from list:");
-                        Console.WriteLine(books[1]);
-
-                        Console.WriteLine("\nCompare two books by ISBN: ");
-                        Console.WriteLine("Book1 isbn({0}) equals book2 isbn({1})?: {2}\n", books[0].getIsbn(),
-                            books[1].getIsbn(),
-                            books[0].Equals(books[1]));
+                        bookObject();
 
                         break;
                     case 3:
-                        AtmMachine machine = new AtmMachine();
-                        var bankOperation = -1;
-                        do
-                        {
-                            Console.WriteLine("Please choose an operation: ");
-                            Console.WriteLine("(1) Check Balance");
-                            Console.WriteLine("(2) Deposit Money");
-                            Console.WriteLine("(3) Withdraw Money");
-                            Console.WriteLine("(4) Exit");
-                            bankOperation = int.Parse(Console.ReadLine());
-
-                            switch (bankOperation)
-                            {
-                                case 1:
-                                    machine.checkBalance();
-                                    break;
-                                case 2:
-                                    Console.WriteLine("Please enter the amount to Deposit: ");
-                                    var depositAmount = double.Parse(Console.ReadLine());
-                                    machine.depositMoney(depositAmount);
-
-                                    break;
-                                case 3:
-                                    Console.WriteLine("Please enter the amount to Deposit: ");
-                                    var withdrawAmount = double.Parse(Console.ReadLine());
-                                    machine.withdrawMoney(withdrawAmount);
-
-                                    break;
-                                default:
-                                    break;
-                            } 
-                        } while (bankOperation!=4);
+                        atmMachine();
 
                         break;
                     case 4:
-                        Console.WriteLine("How many students do you have?: ");
-                        var numStudents = int.Parse(Console.ReadLine());
-                        StudentGrades grades = new StudentGrades();
-                        
-                        for (int i = 1; i <= numStudents; i++)
-                        {
-                            Console.WriteLine("Enter the student's {0} Score:", i);
-                            float studentScore = float.Parse(Console.ReadLine());
-                            grades.addGrade(studentScore);
-                        }
-                       
-                        grades.calculateAvg();
-                        Console.WriteLine("Student's Grades:");
-                        grades.displayGrades();
+                        studentAVGCalc();
 
                         break;
                     case 5:
-                        Console.WriteLine("Enter the First Date (MM/DD/YYYY): ");
-                        DateTime date1 = DateTime.Parse(Console.ReadLine());
-
-                        Console.WriteLine("Enter the Second Date (MM/DD/YYYY): ");
-                        DateTime date2 = DateTime.Parse(Console.ReadLine());
-
-                        var differenceDays = (date1 - date2).TotalDays;
-                        Console.WriteLine("There are {0} days between ({1}) and ({2})\n",
-                            differenceDays < 0? differenceDays * -1 : differenceDays,
-                            date1.ToString("dd/MM/yyyy"), date2.ToString("dd/MM/yyyy"));
+                        compareDates();
 
                         break;
                     case 6:
                         Console.WriteLine("Enter your text:");
-                        var text = Console.ReadLine();
+                        var text = Console.Read();
+                        Console.WriteLine(text);    
+
                         break;
                     case 7:
+                        fileSorter.sortFile();
 
                         break;
                     default:
                         break;
                 } 
             } while (option != 8);
+
+            static void compareDates()
+            {
+                Console.WriteLine("Enter the First Date (MM/DD/YYYY): ");
+                DateTime date1 = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the Second Date (MM/DD/YYYY): ");
+                DateTime date2 = DateTime.Parse(Console.ReadLine());
+
+                var differenceDays = (date1 - date2).TotalDays;
+                Console.WriteLine("There are {0} days between ({1}) and ({2})\n",
+                    differenceDays < 0 ? differenceDays * -1 : differenceDays,
+                    date1.ToString("dd/MM/yyyy"), date2.ToString("dd/MM/yyyy"));
+            }
+
+            static void bookObject()
+            {
+                List<Book> books = new List<Book>();
+
+                Book book1 = new Book("Moby-Dick", "Herman Melville", "123987");
+                Book book2 = new Book("The Lord of the Rings", "John Ronald", "569032");
+                Book book3 = new Book("The Lord of the Rings", "Jhonny Wonka", "569032");
+
+                book1.bookDetails();
+
+                books.Add(book1);
+                books.Add(book2);
+                books.Add(book3);
+                Console.WriteLine("List of books:");
+                books.ForEach(p => p.bookDetails());
+
+                Console.WriteLine("After Removing 1 element:");
+                books.RemoveAt(0);
+                books.ForEach(p => p.bookDetails());
+
+                Console.WriteLine("\nSingle book from list:");
+                Console.WriteLine(books[1]);
+
+                Console.WriteLine("\nCompare two books by ISBN: ");
+                Console.WriteLine("Book1 isbn({0}) equals book2 isbn({1})?: {2}\n", books[0].getIsbn(),
+                    books[1].getIsbn(),
+                    books[0].Equals(books[1]));
+            }
+
+            static void atmMachine()
+            {
+                AtmMachine machine = new AtmMachine();
+                var bankOperation = -1;
+                do
+                {
+                    Console.WriteLine("Please choose an operation: ");
+                    Console.WriteLine("(1) Check Balance");
+                    Console.WriteLine("(2) Deposit Money");
+                    Console.WriteLine("(3) Withdraw Money");
+                    Console.WriteLine("(4) Exit");
+                    bankOperation = int.Parse(Console.ReadLine());
+
+                    switch (bankOperation)
+                    {
+                        case 1:
+                            machine.checkBalance();
+                            break;
+                        case 2:
+                            Console.WriteLine("Please enter the amount to Deposit: ");
+                            var depositAmount = double.Parse(Console.ReadLine());
+                            machine.depositMoney(depositAmount);
+
+                            break;
+                        case 3:
+                            Console.WriteLine("Please enter the amount to Deposit: ");
+                            var withdrawAmount = double.Parse(Console.ReadLine());
+                            machine.withdrawMoney(withdrawAmount);
+
+                            break;
+                        default:
+                            break;
+                    }
+                } while (bankOperation != 4);
+            }
+        }
+
+        private static void studentAVGCalc()
+        {
+            Console.WriteLine("How many students do you have?: ");
+            var numStudents = int.Parse(Console.ReadLine());
+            StudentGrades grades = new StudentGrades();
+
+            for (int i = 1; i <= numStudents; i++)
+            {
+                Console.WriteLine("Enter the student's {0} Score:", i);
+                float studentScore = float.Parse(Console.ReadLine());
+                grades.addGrade(studentScore);
+            }
+
+            grades.calculateAvg();
+            Console.WriteLine("Student's Grades:");
+            grades.displayGrades();
         }
     }
 }
---
+++ Practice1/fileSorter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    public class fileSorter
+    {
+        public static void sortFile()
+        {
+            Console.WriteLine("Enter File Path including File name (.../text.txt):");
+            string path = Console.ReadLine();
+            string[] lines = File.ReadAllLines(path);
+
+            Console.WriteLine("File Content: ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Array.Sort(lines);
+
+            Console.WriteLine("\nAfter Sorting File:");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Random random = new Random();
+            int randomNumber = random.Next();
+            string dir = Path.GetDirectoryName(path);
+            string ext = Path.GetExtension(path);
+            var newPath = Path.Combine(dir, Path.GetRandomFileName() + ext);
+
+            //FileStream fs = File.Create(newPath);
+            File.WriteAllLines(newPath, lines);
+
+            Console.WriteLine("New File created, Path: {0}\n", newPath);
+        }
+    }
+}
